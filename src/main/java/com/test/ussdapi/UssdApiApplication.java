8f8eb5652223f3603aff58929bbe83b265cc4bf5
thendokhae <thendo.makahane@itq.co.za>
2021-03-24 18:45:36 +0200

    spring initialize

---
+++ src/main/java/com/test/ussdapi/UssdApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.ussdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ussdapi/UssdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ussdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
